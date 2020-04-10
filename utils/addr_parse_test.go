59f3d677f549fa29e0cf860f9408e3c16f29d37b
xsguo <xsguo@aibee.com>
2020-04-10 16:25:50 +0800

    test: add parseservicename

--- utils/addr_parse_test.go
+++ utils/addr_parse_test.go
@@ -1,11 +1,17 @@
 package utils
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestParseServicePath(t *testing.T) {
 	_, _, err := ParseServicePath("Greeter.Hello")
-	assert.NotNil(t,err)
+	assert.NotNil(t, err)
+
+	seriveName, method, err := ParseServicePath("SerivceName/Method")
+	assert.Equal(t, seriveName, "erivceName")
+	assert.Equal(t, method, "Method")
+	assert.Equal(t, err, nil)
 }
