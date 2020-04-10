3eb74bac281b9bfd5e3ba685ed086b0d75d64218
lubanproj <1811704358@qq.com>
2020-04-10 17:54:11 +0800

    refactor(utils): modify addr_parse : add serviceName prefix judgement

--- utils/addr_parse.go
+++ utils/addr_parse.go
@@ -25,7 +25,7 @@ func ParseAddress(target string) (string, string, error){
 // parse service path
 func ParseServicePath(path string) (string, string, error) {
 	index := strings.LastIndex(path, "/")
-	if index == 0 || index == -1 {
+	if index == 0 || index == -1 || !strings.HasPrefix(path, "/"){
 		return "", "" , errors.New("invalid path")
 	}
 	return path[1:index], path[index+1:], nil
--- utils/addr_parse_test.go
+++ utils/addr_parse_test.go
@@ -10,8 +10,11 @@ func TestParseServicePath(t *testing.T) {
 	_, _, err := ParseServicePath("Greeter.Hello")
 	assert.NotNil(t, err)
 
-	seriveName, method, err := ParseServicePath("SerivceName/Method")
-	assert.Equal(t, seriveName, "erivceName")
-	assert.Equal(t, method, "Method")
+	_, _, err = ParseServicePath("Greeter/Hello")
+	assert.NotNil(t, err)
+
+	seriveName, method, err := ParseServicePath("/serivceName/method")
+	assert.Equal(t, seriveName, "serivceName")
+	assert.Equal(t, method, "method")
 	assert.Equal(t, err, nil)
 }
