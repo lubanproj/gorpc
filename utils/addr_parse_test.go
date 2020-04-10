c908ddd6d9fe0fcab9f78942f2c0a0a208855e46
lubanproj <1811704358@qq.com>
2020-04-10 17:55:19 +0800

    refactor(utils): modify utils test

--- utils/addr_parse_test.go
+++ utils/addr_parse_test.go
@@ -13,8 +13,8 @@ func TestParseServicePath(t *testing.T) {
 	_, _, err = ParseServicePath("Greeter/Hello")
 	assert.NotNil(t, err)
 
-	seriveName, method, err := ParseServicePath("/serivceName/method")
-	assert.Equal(t, seriveName, "serivceName")
-	assert.Equal(t, method, "method")
+	seriveName, method, err := ParseServicePath("/Greeter/Hello")
+	assert.Equal(t, seriveName, "Greeter")
+	assert.Equal(t, method, "Hello")
 	assert.Equal(t, err, nil)
 }
