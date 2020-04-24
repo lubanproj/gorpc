6ca216f23392ea8f47e3d355198e0d28a7ee1696
lubanproj <1811704358@qq.com>
2020-04-24 19:52:45 +0800

    feat(codec): add codec test

---
+++ codec/codec_test.go
@@ -0,0 +1,19 @@
+package codec
+
+import "testing"
+
+func TestRegisterCodec(t *testing.T) {
+
+}
+
+func TestGetCodec(t *testing.T) {
+
+}
+
+func TestDefaultCodec_Decode(t *testing.T) {
+
+}
+
+func TestDefaultCodec_Encode(t *testing.T) {
+
+}
