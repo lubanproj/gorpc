10b2dba0391cf1afbf7605afddb67664bbde555c
lubanproj <1811704358@qq.com>
2020-04-24 23:52:55 +0800

    refactor(codec): modify codec test

--- codec/codec_test.go
+++ codec/codec_test.go
@@ -1,14 +1,17 @@
 package codec
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 func TestRegisterCodec(t *testing.T) {
+	RegisterCodec("testCodec", nil)
 
+	codec := GetCodec("testCodec")
+	assert.Equal(t, codec, nil)
 }
 
-func TestGetCodec(t *testing.T) {
-
-}
 
 func TestDefaultCodec_Decode(t *testing.T) {
 
