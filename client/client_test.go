c59379522eef8a8c70de61634d98f0a27ea06e29
lubanproj <1811704358@qq.com>
2020-04-26 13:20:41 +0800

    test(client): modify client test

--- client/client_test.go
+++ client/client_test.go
@@ -18,11 +18,10 @@ func TestCall(t *testing.T) {
 	wg.Add(1)
 	go func() {
 		serverOpts := []gorpc.ServerOption{
-			gorpc.WithAddress("127.0.0.1:8000"),
+			gorpc.WithAddress("localhost:8000"),
 			gorpc.WithNetwork("tcp"),
 			gorpc.WithSerializationType("msgpack"),
 			gorpc.WithTimeout(time.Millisecond * 2000),
-			gorpc.WithSelectorSvrAddr("localhost:8500"),
 		}
 		s := gorpc.NewServer(serverOpts ...)
 		if err := s.RegisterService("helloworld.Greeter", new(helloworld.Service)); err != nil {
@@ -41,7 +40,7 @@ func TestCall(t *testing.T) {
 	wg.Wait()
 
 	opts := []Option {
-		WithTarget("127.0.0.1:8000"),
+		WithTarget("localhost:8000"),
 		WithNetwork("tcp"),
 		WithTimeout(2000 * time.Millisecond),
 		WithSerializationType("msgpack"),
