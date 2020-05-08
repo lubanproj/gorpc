ad72ac898dcc43f3269a505e4fae9f021eb5d00b
lubanproj <1811704358@qq.com>
2020-05-08 12:30:46 +0800

    feat(examples): add auth examples

--- examples/auth/client/client.go
+++ examples/auth/client/client.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/lubanproj/gorpc/auth"
 	"time"
 
 	"github.com/lubanproj/gorpc/client"
@@ -11,10 +12,11 @@ import (
 
 func main() {
 	opts := []client.Option {
-		client.WithTarget("127.0.0.1:8000"),
+		client.WithTarget("127.0.0.1:8003"),
 		client.WithNetwork("tcp"),
-		client.WithTimeout(2000 * time.Millisecond),
+		client.WithTimeout(2000000 * time.Millisecond),
 		client.WithSerializationType("msgpack"),
+		client.WithPerRPCAuth(auth.NewOAuth2ByToken("testToken")),
 	}
 	c := client.DefaultClient
 	req := &testdata.HelloRequest{
--- examples/auth/server/server.go
+++ examples/auth/server/server.go
@@ -1,19 +1,40 @@
 package main
 
 import (
+	"context"
+	"errors"
+	"github.com/lubanproj/gorpc/log"
 	"time"
 
 	"github.com/lubanproj/gorpc"
+	"github.com/lubanproj/gorpc/auth"
+	"github.com/lubanproj/gorpc/metadata"
 	"github.com/lubanproj/gorpc/testdata"
 )
 
 
 func main() {
+
+	af := func(ctx context.Context) (context.Context, error){
+		md := metadata.ServerMetadata(ctx)
+
+		if len(md) == 0 {
+			return ctx, errors.New("token nil")
+		}
+		v := md["authorization"]
+		log.Debug("token : ", string(v))
+		if string(v) != "Bearer testToken" {
+			return ctx, errors.New("token invalid")
+		}
+		return ctx, nil
+	}
+
 	opts := []gorpc.ServerOption{
-		gorpc.WithAddress("127.0.0.1:8000"),
+		gorpc.WithAddress("127.0.0.1:8003"),
 		gorpc.WithNetwork("tcp"),
 		gorpc.WithSerializationType("msgpack"),
-		gorpc.WithTimeout(time.Millisecond * 2000),
+		gorpc.WithTimeout(time.Millisecond * 2000000),
+		gorpc.WithInterceptor(auth.BuildAuthInterceptor(af)),
 	}
 	s := gorpc.NewServer(opts ...)
 	if err := s.RegisterService("/helloworld.Greeter", new(testdata.Service)); err != nil {
