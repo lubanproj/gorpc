bbaf39129a311cbb3645b3db2d19ffa63a94ae8c
lubanproj <1811704358@qq.com>
2020-05-11 14:33:58 +0800

    test(auth): tls ClientHandshake host name change to helloworld

--- auth/auth_test.go
+++ auth/auth_test.go
@@ -33,7 +33,7 @@ func TestClientHandshake(t *testing.T) {
 	tAuth , err := NewClientTLSAuthFromFile("../testdata/server.crt","helloworld")
 	assert.Nil(t, err)
 	var ctx = context.Background()
-	wrapperConn, _ , err := tAuth.ClientHandshake(ctx, "testAuth",conn)
+	wrapperConn, _ , err := tAuth.ClientHandshake(ctx, "helloworld", conn)
 	assert.Nil(t, err)
 
 	data , err := wrapperConn.Write([]byte("hello\n"))
