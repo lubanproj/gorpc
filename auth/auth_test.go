913720b3a8bc994ccfe38a9e708e426b734794a8
lubanproj <1811704358@qq.com>
2020-05-13 12:17:34 +0800

    test(auth): fix auth_test go master version compile failed

--- auth/auth_test.go
+++ auth/auth_test.go
@@ -2,6 +2,7 @@ package auth
 
 import (
 	"fmt"
+	"net"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -19,58 +20,58 @@ func TestNewClientTLSAuthFromFile(t *testing.T) {
 	fmt.Printf("client conf : %v \n", transportAuth.(*tlsAuth))
 }
 
-//func TestClientHandshake(t *testing.T) {
-//	ch := make(chan int)
-//	go newServer(t, ch)
-//	<- ch
-//	conn , err := net.Dial("tcp","127.0.0.1:8002")
-//	assert.Nil(t, err)
-//	defer conn.Close()
-//	tAuth , err := NewClientTLSAuthFromFile("../testdata/server.crt","lubanstudio.cn")
-//	assert.Nil(t, err)
-//	var ctx = context.Background()
-//	wrapperConn, _ , err := tAuth.ClientHandshake(ctx, "lubanstudio.cn", conn)
-//	assert.Nil(t, err)
-//
-//	data , err := wrapperConn.Write([]byte("hello\n"))
-//	assert.Nil(t, err)
-//
-//	buf := make([]byte, 100)
-//	data, err = wrapperConn.Read(buf)
-//	assert.Nil(t, err)
-//
-//	fmt.Println(string(buf[:data]))
-//}
-//
-//func newServer(t *testing.T, ch chan int) {
-//	ln, err := net.Listen("tcp",":8002")
-//	assert.Nil(t, err)
-//	ch <- 1
-//	defer ln.Close()
-//
-//	for {
-//		conn, err := ln.Accept()
-//		assert.Nil(t, err)
-//		tlsAuth , err := NewServerTLSAuthFromFile("../testdata/server.crt","../testdata/server.key")
-//		assert.Nil(t, err)
-//		wrapperConn, _, err := tlsAuth.ServerHandshake(conn)
-//		assert.Nil(t, err)
-//		go handleConn(t,wrapperConn)
-//	}
-//}
-//
-//func handleConn(t *testing.T, conn net.Conn) {
-//	defer conn.Close()
-//	r := bufio.NewReader(conn)
-//
-//	for {
-//		msg, err := r.ReadString('\n')
-//		if err != nil {
-//			if err != io.EOF {
-//				assert.Nil(t, err)
-//			}
-//		}
-//		fmt.Println(msg)
-//		conn.Write([]byte("world\n"))
-//	}
-//}
+func TestClientHandshake(t *testing.T) {
+	ch := make(chan int)
+	go newServer(t, ch)
+	<- ch
+	conn , err := net.Dial("tcp","127.0.0.1:8002")
+	assert.Nil(t, err)
+	defer conn.Close()
+	tAuth , err := NewClientTLSAuthFromFile("../testdata/server.crt","lubanstudio.cn")
+	assert.Nil(t, err)
+	var ctx = context.Background()
+	wrapperConn, _ , err := tAuth.ClientHandshake(ctx, "lubanstudio.cn", conn)
+	assert.Nil(t, err)
+
+	data , err := wrapperConn.Write([]byte("hello\n"))
+	assert.Nil(t, err)
+
+	buf := make([]byte, 100)
+	data, err = wrapperConn.Read(buf)
+	assert.Nil(t, err)
+
+	fmt.Println(string(buf[:data]))
+}
+
+func newServer(t *testing.T, ch chan int) {
+	ln, err := net.Listen("tcp",":8002")
+	assert.Nil(t, err)
+	ch <- 1
+	defer ln.Close()
+
+	for {
+		conn, err := ln.Accept()
+		assert.Nil(t, err)
+		tlsAuth , err := NewServerTLSAuthFromFile("../testdata/server.crt","../testdata/server.key")
+		assert.Nil(t, err)
+		wrapperConn, _, err := tlsAuth.ServerHandshake(conn)
+		assert.Nil(t, err)
+		go handleConn(t,wrapperConn)
+	}
+}
+
+func handleConn(t *testing.T, conn net.Conn) {
+	defer conn.Close()
+	r := bufio.NewReader(conn)
+
+	for {
+		msg, err := r.ReadString('\n')
+		if err != nil {
+			if err != io.EOF {
+				assert.Nil(t, err)
+			}
+		}
+		fmt.Println(msg)
+		conn.Write([]byte("world\n"))
+	}
+}
